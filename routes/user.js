000e978692f8117570cc108034ed9439012c4836
Atharv2445 <atharvbhosale24@gmail.com>
2026-08-21 21:08:56 +0530

    login page added

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const user = require('../models/user');
 const router=express.Router();
 const User=require("../models/user.js");
 const WrapAsync = require('../utils/WrapAsync.js');
-
-
+const passport=require("passport");
+//sign-up page
 router.get("/signup",(req,res)=>{
 res.render("users/signup.ejs");
 
@@ -34,4 +34,27 @@ router.post("/signup",WrapAsync(async(req,res)=>{
 
 
 }}));
+ 
+//login page
+
+router.get("/login",(req,res)=>{
+
+
+     res.render("users/login.ejs");
+
+
+
+});
+
+router.post("/login", passport.authenticate('local', { failureRedirect: '/login',failureFlash:true }),
+(req,res)=>{
+  req.flash("success","Welcome back to Wanderlust! You are logged in!")
+  res.redirect("/listings");
+
+})
+
+
+
+
+
 module.exports=router;
